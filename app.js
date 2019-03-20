4cbff066096a393f8a250cc624b7d89bacae6242
Sergei Vasilev <Sergei_Vasielv1@epam.com>
2019-03-21 00:57:23 +0300

    Task-5 beta

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import * as config from "./config/config.json";
 import { User, Product, DirWatcher, Importer } from "./modules";
+import jwt from "jsonwebtoken";
 import cookieParser from "./midldewares/cokieparser";
 import queryParser from "./midldewares/queryparser";
+import jwtVerifyer from "./midldewares/jwtverifyer";
+
 
 const app = express();
 
@@ -13,42 +16,84 @@ app.use(cookieParser());
 
 //  temporary place for routes
 
-app.get("/api/products", (req, res) => {
+app.get("/api/products", jwtVerifyer, (req, res) => {
   res.send("Returning ALL products");
   res.end();
 });
 
-app.get("/api/products/:id", (req, res) => {
+app.get("/api/products/:id", jwtVerifyer, (req, res) => {
   const { id } = req.params;
   res.send(`Return SINGLE product with ID: ${id}`);
 
   res.end();
 });
 
-app.get("/api/products/:id/reviews", (req, res) => {
+app.get("/api/products/:id/reviews", jwtVerifyer, (req, res) => {
   const { id } = req.params;
   res.send(`Return ​ALL​ reviews for a single product with ID: ${id}`);
   res.end();
 });
 
-app.get("/api/users", (req, res) => {
+app.get("/api/users", jwtVerifyer,(req, res) => {
   res.send("Return ALL users");
   res.end();
 });
 
-app.post("/api/products", (req, res) => {
+app.post("/api/products", jwtVerifyer, (req, res) => {
   res.send("Add ​NEW​ product and return it");
   res.end();
 });
 
+const user = {
+    id: '123',
+    userName: 'awsomeUser',
+    email: 'awsome@mail.yep',
+    password: '123',
+};
+
+
+const successfulAuth = {
+    code: ​200​,
+    message: ​"OK"​,​
+    data: {​
+        user: {
+            email: ​"awsome@mail.yep"​,​
+            username: ​"awsomeUser"       
+        }   
+    },
+    token: ​"awsomeToken"
+};
+
+const faluereAuth = {
+    code: ​404​,
+    message: ​"Not Found"​,
+    data: "some error text" 
+};
+
+app.post("/auth", (req, res) => {
+    const isVerifiedName = user.userName === req.parsedQuery.userName;
+    const isVerifiedPassword = user.password === req.parsedQuery.password;
+
+    if(isVerifiedName && isVerifiedPassword) {
+        const token = jwt.sign(user, 'secret')
+
+        res.json({
+            success: true,
+            token
+          });
+    } else {
+        res.status(404).send(JSON.stringify(authFailed));
+    }
+})
+
 export default app;
 
-console.log(config.name);
-new User();
-new Product();
+// console.log(config.name);
+// new User();
+// new Product();
 
-const importer = new Importer();
-const watcher = new DirWatcher();
+// const importer = new Importer();
+// const watcher = new DirWatcher();
 // watcher.watch(config.csvFolder, 1000)
 // importer.listen(watcher)
 // // importer.import(`${config.csvFolder}/MOCK_DATA-3.csv`)
